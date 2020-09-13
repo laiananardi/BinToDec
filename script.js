0952e9caa829994f10607efac0fd696f7e870c49
laiananardi <54119801+laiananardi@users.noreply.github.com>
2020-09-13 14:50:27 -0300

    dec to bin

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 
+const first = document.getElementById('first')
+const second = document.getElementById('second')
+const num = document.getElementById('num')
+
+
 function convert() {
-    const bin = document.getElementById('bin').value;
-    if (bin === ''){
-        return alert('Please, enter a binary number');
-    } else{
-            bin.split('').map((char) => {
-                if (char !== '0' && char !== '1'){
+
+    if(first.value == '' || second.value == '' || num.value == ''){
+        alert('Please, fill in all fields! ')
+        first.value =  ''
+        second.value = ''
+        num.value = ''
+    }else if(first.value == 'bin' && second.value == 'dec'){
+        num.value.split('').map((char) => {
+            if (char !== '0' && char !== '1'){
                 return alert('Please, enter a binary number');
-                } else{
-                    const dec = parseInt(bin, 2);
-                    document.getElementById('dec').value = dec;
-                    return true;
-                }
-            });
-        }
+            } else{
+                var ans = parseInt(num.value, 2);
+                document.getElementById('ans').value = `Binary: ${num.value} | Decimal: ${ans}`;
+                return true;
+            }
+        });
+    }else if(first.value == 'dec' && second.value == 'bin'){
+        var ans = Number(num.value).toString(2)
+        document.getElementById('ans').value = ans;
+        return true;
+    }else{
+        alert('Please, select different bases .')
+        first.value =  ''
+        second.value = ''
+        num.value = ''
+    }
 }
+
+
 
